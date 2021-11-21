dcc22276bc14b54a043ede63c1daa1271a91a211
Guilherme Duarte <guiduarte998@gmail.com>
2021-11-21 18:28:26 +0000

    CSS, js modal

--- StoreLayout/DefaultStore/wwwroot/js/storeView.js
+++ StoreLayout/DefaultStore/wwwroot/js/storeView.js
@@ -1,25 +1,17 @@
-﻿// Get the modal
-var modal = document.getElementById("myModal");
+﻿let modalBtn = document.getElementById("modal-product")
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+let modal = document.querySelector(".modal")
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+let closeBtn = document.querySelector(".close-btn")
 
-// When the user clicks the button, open the modal 
-btn.onclick = function () {
-    modal.style.display = "block";
+modalBtn.onclick = function () {
+    modal.style.display = "block"
 }
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-    modal.style.display = "none";
+closeBtn.onclick = function () {
+    modal.style.display = "none"
 }
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+window.onclick = function (e) {
+    if (e.target == modal) {
+        modal.style.display = "none"
     }
 }
